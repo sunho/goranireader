43e54b001b6cd464af8d2d42c74c210e65845409
sunho <ksunhokim123@naver.com>
2019-04-04 13:22:51 +0900

    asfd

--- backend/dataserver/gorani/shared/tfmodels.py
+++ backend/dataserver/gorani/shared/tfmodels.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras.layers import LSTM, Dense, Input
 
-class TimeModel(tf.keras.Sequential):
+class TimeModel(tf.keras.Model):
     def __init__(self, feature_length: int):
-        super(TimeModel, self).__init__()
+        super(TimeModel, self).__init__(name='Time Model')
         self.feature_length = feature_length
-        self.add(Input(shape=(None, feature_length)))
-        self.add(LSTM(256))
-        self.add(Dense(128))
-        self.add(Dense(1, activation='linear'))
+        self.lstm = LSTM(32)
+        self.dense1 = Dense(32, activation='relu')
+        self.dense2 = Dense(1)
+
+    def call(self, inputs):
+        x = self.lstm(inputs)
+        x = self.dense1(x)
+        return self.dense2(x)
