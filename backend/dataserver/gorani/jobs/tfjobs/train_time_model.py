f058b24a4a5f19814b9474a138ccccc19785e6f3
sunho <ksunhokim123@naver.com>
2019-04-06 11:31:56 +0900

    gg

--- backend/dataserver/gorani/jobs/tfjobs/train_time_model.py
+++ backend/dataserver/gorani/jobs/tfjobs/train_time_model.py
@@ -1,6 +1,8 @@
 from gorani.shared import TFJob, TFJobContext
 from . import PrepareTimeDataset
+from .prepare_time_dataset import FEATURE_LEN
 from gorani.shared.tfmodels import TimeModel
+from tensorflow import keras
 
 class TrainTimeModel(TFJob):
     def __init__(self, context: TFJobContext):
@@ -8,8 +10,10 @@ class TrainTimeModel(TFJob):
 
     def train(self):
         job = PrepareTimeDataset(self.context)
+        cb = keras.callbacks.TensorBoard(log_dir='/gorani/graph', histogram_freq=0,
+          write_graph=True, write_images=True)
         ps = self.context.data_db.get_flipped_paragraphs(1)
         x, y = job.transform_paragraphs(ps)
-        model = TimeModel()
-        model.compile(loss='mse', optimizer='adam')
-        model.fit(X, y, epochs=1000, verbose=1)
+        model = TimeModel(FEATURE_LEN)
+        model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        model.fit(x, y, batch_size = 60, epochs=1000, verbose=1, callbacks=[cb], validation_data = (x_test, y_test))
--- backend/deploy/send.sh
+++ backend/deploy/send.sh
@@ -2,6 +2,6 @@ set -e
 
 . ./deploy/env.sh
 
-./deploy/ssh.sh "rm -rf $GORANI_HOME$@"
+./deploy/ssh.sh "sudo rm -rf $GORANI_HOME$@"
 scp -r "$PWD$@" "$SERVER:$GORANI_HOME$@"
 ./deploy/ssh.sh "chmod -R 770 $GORANI_HOME$@"
