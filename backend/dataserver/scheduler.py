30047896ba131039906e58c9cbecf7032f73e6c4
Sunho Kim <ksunhokim123@naver.com>
2019-06-03 18:07:46 +0900

    asdf

--- backend/dataserver/scheduler.py
+++ backend/dataserver/scheduler.py
@@ -1,7 +1,23 @@
-import papermill as pm
+import nbformat
+from nbconvert import HTMLExporter
 
-pm.execute_notebook(
-   'jobs/cluster-books.ipynb',
-   'out/output.ipynb',
-   parameters = dict(alpha=0.6, ratio=0.1)
-)
+nb = nbformat.read('../output.ipynb', as_version=4)
+for cell in nb.cells:
+    cell.metadata['hide_input'] = True
+
+html_exporter = HTMLExporter()
+html_exporter.template_file = 'basic'
+(body, resources) = html_exporter.from_notebook_node(nb)
+html_file_writer = open('asdf.html', 'wb')
+html_file_writer.write(body.encode('utf-8'))
+html_file_writer.close()
+
+# import papermill as pm
+
+# pm.execute_notebook(
+    # 'jobs/cluster-books.ipynb',
+    # 'out/output.ipynb',
+    # parameters = dict(alpha=0.6, ratio=0.1)
+# )
+
+
