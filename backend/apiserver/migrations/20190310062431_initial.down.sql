5fd9a749e13df0030c1ee037d96b6d3e4e2adc73
Sunho Kim <ksunhokim123@naver.com>
2019-08-12 21:23:15 +0900

    Add firebae

--- backend/apiserver/migrations/20190310062431_initial.down.sql
+++
@@ -1,8 +0,0 @@
-drop table classes;
-drop table books;
-drop table users;
-drop table memories;
-drop table class_missions;
-drop table user_book_progresses;
-drop table admins;
-drop table admins_classes;
--- backend/apiserver/utils/M.go
+++
@@ -1,7 +0,0 @@
-//
-// Copyright © 2019 Sunho Kim. All rights reserved.
-//
-
-package utils
-
-type M map[string]interface{}
