f66cd4608223eafb616490f796214e3ca77f2365
Sunho Kim <ksunhokim123@naver.com>
2019-09-10 20:20:52 +0900

    [ios] add login

--- frontend/ios/App/UI/Auth/LoginViewController.swift
+++ frontend/ios/App/UI/Auth/LoginViewController.swift
@@ -6,7 +6,19 @@ import BLTNBoard
 import UIKit
 
 class LoginViewController: UIViewController  {
+    var loginForm: LoginBulletPageManager!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginForm = LoginBulletPageManager()
+        loginForm.callback = checkAuth
+        checkAuth()
+    }
+    
+    @IBAction func getStarted(_ sender: Any) {
+        loginForm.show(above: self)
+    }
+    
+    func checkAuth() {
+        
     }
 }
