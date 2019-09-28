fd60dee486b72dcfb8a5a3a2584530661b75395f
Sunho Kim <ksunhokim123@naver.com>
2019-09-28 22:15:23 +0900

    Clean

--- backend/dataserver/pipeline/init.sh
+++
@@ -1,5 +0,0 @@
-/opt/conda/bin/pip install firebase-admin
-/opt/conda/bin/pip install dateutil
-/opt/conda/bin/pip install iso8601
-/opt/conda/bin/pip install gcloud
-/opt/conda/bin/pip install reportng
