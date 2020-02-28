e717bf7547fcb7fbb5c758ef07351198b5bad162
sunho <sunho@sunhos-MacBook-Pro.local>
2020-02-28 19:47:24 +0900

    update

---
+++ frontend/app/components/Firebase/context.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Firebase from '.';
+
+const FirebaseContext = React.createContext<Firebase | null>(null);
+
+export interface FirbaseProps {
+    firebase: Firebase;
+}
+
+export const withFirebase = (Component: any) => (props: any) => (
+    <FirebaseContext.Consumer>
+      {firebase => <Component {...props} firebase={firebase} />}
+    </FirebaseContext.Consumer>
+);
+
+export default FirebaseContext;
