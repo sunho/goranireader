ea77da017c3b944cc19ea65a8a1751af59f1dc1b
Sunho Kim <ksunhokim123@naver.com>
2020-03-20 03:29:10 +0900

    Remove old clients

--- frontend/android/app/src/main/java/kim/sunho/goranireader/extensions/utils.kt
+++
@@ -1,8 +0,0 @@
-package kim.sunho.goranireader.extensions
-
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-
-fun JsonDefault(): Json {
-    return Json(JsonConfiguration.Stable)
-}
--- frontend/android/app/src/main/java/kim/sunho/goranireader/models/Memory.kt
+++
@@ -1,9 +0,0 @@
-package kim.sunho.goranireader.models
-
-import com.google.firebase.Timestamp
-import java.util.*
-
-data class Memory(
-    val createdAt: Timestamp = Timestamp(Date()),
-    val sentence: String = ""
-)
