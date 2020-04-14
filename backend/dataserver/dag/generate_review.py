34916fe1f66f67b424c1c3afe251fbccb8829fab
Sunho Kim <ksunhokim123@naver.com>
2020-04-14 19:58:41 +0900

    [dataserver] Add generate review dag

--- backend/dataserver/dag/generate_review.py
+++ backend/dataserver/dag/generate_review.py
@@ -1,95 +1,78 @@
-import boto3
-from botocore.exceptions import ClientError
-from dataserver.utils import split_sentence, unnesting
-from metaflow import FlowSpec, step, Flow, IncludeFile, conda_base
-import pandas as pd
-import json
-import uuid
-import typing
-import numpy as np
-from dataserver.booky import Book
-from pandas import DataFrame
-from send import log_msg
-import time
-from datetime import datetime, timedelta
-from pytz import timezone, utc
-import pandera as pa
-from dataserver.utils import pip
+from metaflow import FlowSpec, step, IncludeFile, conda_base, Flow
 
-@conda_base(libraries= {
-    'boto3': '1.12',
-    'booky': '1.0.5',
-    'pandera': '0.3.2',
-    'rsa': '4.0'
-})
+from dataserver.job.review import decide_review_words, decide_target_words, serialize_review_words_df, \
+    serialize_stats_df, combine_serialized_dfs
+from dataserver.models.vocab_skill import VocabSkill
+from dataserver.job.stats import extract_session_info_df, extract_last_session_df, calculate_vocab_skills
+from dataserver.service.notification import NotificationService
+from dag.decorators import pip
+from dag.deps import deps
+import time
+
+from dataserver.models.config import Config
+from dataserver.service.s3 import S3Service
+from dataserver.service.user import UserService
+
+
+@conda_base(libraries= deps)
 class GenerateReview(FlowSpec):
-    firebase_key = IncludeFile(
-        'firebase-key',
+    config_file = IncludeFile(
+        'config',
         is_text=False,
-        help='Firebase Key File',
-        default='./firebase-key.json')
+        help='Config Key File',
+        default='./config.yaml')
 
     @step
     def start(self):
         flow = Flow('DownloadLog').latest_successful_run
-        print('using data from flow: %s' % flow.id)
+        print('using users data from flow: %s' % flow.id)
+
         self.users = flow.data.users
 
-        flow = Flow('PreprocessPaginate').latest_successful_run
-        print('using data from flow: %s' % flow.id)
+        flow = Flow('GenerateStats').latest_successful_run
+        print('using users data from flow: %s' % flow.id)
+
+        self.last_session_df = flow.data.last_session_df
         self.session_info_df = flow.data.session_info_df
+
+        flow = Flow('LoadMetadata').latest_successful_run
+        print('using vocab skills data from flow: %s' % flow.id)
+
+        self.vocab_skills = flow.data.vocab_skills
+
+        flow = Flow('PredictVocab').latest_successful_run
+        print('using vocab data from flow: %s' % flow.id)
+
+        self.unknown_words_df = flow.data.unknown_words_df
+
+        flow = Flow('PreprocessPaginate').latest_successful_run
+        print('using signals data from flow: %s' % flow.id)
+
         self.clean_pages_df = flow.data.clean_pages_df
-        self.signals_df = flow.data.signals_df
-        self.last_words_after_hours = 12
-        self.skip_session_hours = 0.1
-        self.last_stats_days = 14
 
-        self.next(self.preprocess)
+        import yaml
+        self.config = Config(**yaml.load(self.config_file))
+
+        self.next(self.generate_review)
 
     @step
-    def preprocess(self):
-        pass
+    def generate_review(self):
+        words_df = decide_review_words(self.unknown_words_df, self.vocab_skills, self.last_session_df, 24*4)
+        target_df = decide_target_words(words_df)
 
-    @pip(libraries={
-        'firebase-admin': '4.0.1'
-    })
-    @step
-    def upload(self):
-        import firebase_admin
-        from firebase_admin import credentials
-        from firebase_admin import firestore
-        with open('/tmp/firebase-key.json', 'wb') as f:
-            f.write(self.firebase_key)
-        cred = credentials.Certificate('/tmp/firebase-key.json')
-        firebase_admin.initialize_app(cred, {
-            'projectId': "booky-reader-249509"
-        })
-        out = []
-        db = firebase_admin.firestore.client()
-        bucket_name = 'gorani-reader-generated-reviews'
-        s3 = boto3.resource("s3")
-        for _, row in self.review_df.iterrows():
-            review = json.loads(row['review'])
-            filename = row['userId'] + '-' + str(review['end']) + '.json'
-            try:
-                s3.Object(bucket_name, filename).load()
-            except ClientError as e:
-                if e.response['Error']['Code'] == "404":
-                    out.append(row['userId'])
-                    s3.Object(bucket_name, filename).put(Body=json.dumps(review))
-                    url = "https://" + bucket_name + ".s3.ap-northeast-2.amazonaws.com/" + filename
-                    ref = db.collection('users').document(row['userId'])
-                    ref.set({
-                        'review': url
-                    }, merge=True)
-                else:
-                    raise
-        self.added = out
+        review_words_df = serialize_review_words_df(words_df, self.clean_pages_df, target_df)
+        session_info_df = self.session_info_df
+        session_info_df = session_info_df.loc[(time.time() - 14*24*60*60) <= session_info_df['start']]
+        stats_df = serialize_stats_df(session_info_df)
+        self.review_df = combine_serialized_dfs(stats_df, review_words_df, self.last_session_df, session_info_df)
+
         self.next(self.end)
 
     @step
     def end(self):
-        log_msg('./generate_review.py', '\n'.join(self.added), False)
+        service = NotificationService(self.config)
+        service.complete_flow("Generate Review", "", False)
+
 
 if __name__ == '__main__':
     GenerateReview()
