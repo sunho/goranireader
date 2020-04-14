c7f47d865927a6eeb93d62847dc74a27e1879807
Sunho Kim <ksunhokim123@naver.com>
2020-04-14 17:46:54 +0900

    [dataserver] Add vocab skill calculation

--- backend/dataserver/dataserver/models/config.py
+++ backend/dataserver/dataserver/models/config.py
@@ -12,3 +12,6 @@ class Config(NamedTuple):
     client_event_logs_s3_bucket: str
     generated_review_s3_bucket: str
     vocab_skills_s3_bucket: str
+
+    last_session_after_hours: float
+    skip_session_hours: float
