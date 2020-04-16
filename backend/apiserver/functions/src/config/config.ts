165bdb74a246e040a6b50edb5b5b9843f7612bc5
SkyLightQP <combbm@naver.com>
2020-04-16 21:46:55 +0900

    [apiserver] Add oauth config items

--- backend/apiserver/functions/src/config/config.ts
+++ backend/apiserver/functions/src/config/config.ts
@@ -9,7 +9,18 @@ const config = {
     DB_USERNAME: process.env.DB_USERNAME || 'username',
     DB_PASSWORD: process.env.DB_PASSWORD || 'password',
     DB_HOST: process.env.DB_HOST || '127.0.0.1',
-    DB_PORT: parseInt(process.env.DB_PORT || '3306', 10)
+    DB_PORT: parseInt(process.env.DB_PORT || '3306', 10),
+
+    auth: {
+        google: {
+            clientID: process.env.GOOGLE_CLIENTID || 'clientID',
+            clientSecret: process.env.GOOGLE_CLIENTSECRET || 'clientSecret'
+        },
+        naver: {
+            clientID: process.env.NAVER_CLIENTID || 'clientID',
+            clientSecret: process.env.NAVER_CLIENTSECRET || 'clientSecret'
+        }
+    }
 };
 
 export default config;
--- backend/apiserver/functions/src/passport/strategies/naver-strategy.ts
+++ backend/apiserver/functions/src/passport/strategies/naver-strategy.ts
@@ -2,13 +2,14 @@ import passport from 'passport';
 import naver from 'passport-naver';
 import {onSuccess} from "../index";
 import {Vendor} from "../vendor";
+import config from "../../config/config";
 
 const Strategy = naver.Strategy;
 
 export const initNaverStrategy = () => {
     passport.use(new Strategy({
-            clientID: '',
-            clientSecret: '',
+            clientID: config.auth.naver.clientID,
+            clientSecret: config.auth.naver.clientID,
             callbackURL: ''
         }, (accessToken, refreshToken, profile, done) => {
             const email = profile.emails![0].value;
