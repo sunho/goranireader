ad52ffc76a85af8f5ea70d06b37df38bf13411f3
SkyLightQP <combbm@naver.com>
2020-04-16 21:34:20 +0900

    [apiserver] Apply logger system with log4js

--- backend/apiserver/functions/src/index.ts
+++ backend/apiserver/functions/src/index.ts
@@ -3,10 +3,17 @@ import session from 'express-session';
 import {initPassport} from "./passport";
 import config from "./config/config";
 import passport from "passport";
+import log4js from 'log4js';
+import logconfig from './log4js.json';
 
 export const app = express();
+export const logger = log4js.getLogger();
 
-app.use(express.urlencoded({ extended: false }));
+log4js.configure(logconfig);
+
+logger.level = 'ALL';
+
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(session({
